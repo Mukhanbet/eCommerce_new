68627064a34595dc6fac6fd12c3922e3100e678c
Mukhanbet <nurmuhanbet2004@icloud.com>
2024-02-12 23:56:42 +0600

    AWS is created

---
+++ src/main/java/com/example/demo/controllers/StorageController.java
@@ -0,0 +1,38 @@
+package com.example.demo.controllers;
+
+import com.example.demo.service.StorageService;
+import lombok.AllArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/storage")
+public class StorageController {
+    private StorageService storageService;
+
+    @PostMapping("uploadFile")
+    public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile file) {
+        return new ResponseEntity<>(storageService.uploadFile(file), HttpStatus.OK);
+    }
+
+    @GetMapping("/downloadFile/{fileName}")
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName) {
+        byte[] data = storageService.downloadFile(fileName);
+        ByteArrayResource resource = new ByteArrayResource(data);
+        return ResponseEntity
+                .ok()
+                .contentLength(data.length)
+                .header("Content-type", "application/octet-stream")
+                .header("Content-dispositions", "attachment: filename=\"" + fileName + "\"")
+                .body(resource);
+    }
+
+    @GetMapping("/deleteFile/{fileName}")
+    public ResponseEntity<String> deleteFile(@PathVariable String fileName) {
+        return new ResponseEntity<>(storageService.deleteFile(fileName), HttpStatus.OK);
+    }
+}
