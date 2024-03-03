f24dadc98840e2e4194ed631cbf560cfa17b1ccd
Nurmukhanbet <nurmuhanbet2004@icloud.com>
2024-03-03 23:10:49 +0600

    In security some codes fixed

--- src/main/java/com/example/demo/config/JwtFilter.java
+++ src/main/java/com/example/demo/config/JwtFilter.java
@@ -1,5 +1,6 @@
 package com.example.demo.config;
 
+import com.example.demo.exception.NotFoundException;
 import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -7,7 +8,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,10 +19,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.security.SignatureException;
 
 @Component
 @AllArgsConstructor
+@Slf4j
 public class JwtFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
@@ -41,19 +43,13 @@ public class JwtFilter extends OncePerRequestFilter {
             try {
                 userEmail = jwtService.getUserName(jwt); // Это делает нас куче работы  во-первых мы получаем информацию о пользователе мы проверяем пользователя
             } catch (ExpiredJwtException e) {
-                logger.debug("Время жизни токена вышло");
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+//                log.debug("The expiration of token is over");
+                // todo here should be the message for output
             }
         }
 
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-//            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
-//                    jwtService.getRole(jwt), // todo maybe here has problem
-//                    username,
-//                    null
-//            );
-//            SecurityContextHolder.getContext().setAuthentication(token);
-//        }
-//        filterChain.doFilter(request, response);
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
             if (jwtService.isTokenValid(jwt, userDetails)) {
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
@@ -65,41 +61,11 @@ public class JwtFilter extends OncePerRequestFilter {
                         new WebAuthenticationDetailsSource().buildDetails(request)
                 );
                 SecurityContextHolder.getContext().setAuthentication(authToken);
+            } else {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                // todo here should be the message for output
             }
         }
         filterChain.doFilter(request, response);
     }
-
-
-//    @Override
-//    protected void doFilterInternal(
-//            @NonNull HttpServletRequest request,
-//            @NonNull HttpServletResponse response,
-//            @NonNull FilterChain filterChain
-//    ) throws ServletException, IOException {
-//        final String authHeader = request.getHeader("Authorization");
-//        final String token;
-//        final String userEmail;
-//        if (authHeader == null || authHeader.startsWith("Bearer ")) {
-//            filterChain.doFilter(request, response);
-//            return;
-//        }
-//        token = authHeader.substring(7);
-//        userEmail = jwtService.extractUsername(token);
-//        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-//            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-//            if (jwtService.isTokenValid(token, userDetails)) {
-//                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-//                        userDetails,
-//                        null,
-//                        userDetails.getAuthorities()
-//                );
-//                authToken.setDetails(
-//                        new WebAuthenticationDetailsSource().buildDetails(request)
-//                );
-//                SecurityContextHolder.getContext().setAuthentication(authToken);
-//            }
-//        }
-//        filterChain.doFilter(request, response);
-//    }
 }
--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @Entity
@@ -48,7 +49,12 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
+//        if(role == null) {
+//            return Collections.singleton(new SimpleGrantedAuthority("ROLE_DEFAULT"));
+//        }
+//        return Collections.singleton(new SimpleGrantedAuthority("ROLE_" + role.name()));
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+
     }
 
     @Override
